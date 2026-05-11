a9def4b6df606dba248515e49bad8edb8c3f2713
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-05-11 16:14:48 -0300

    Initial commit

---
+++ routes.js
@@ -0,0 +1,21 @@
+const express = require ('express');
+const gamesController = require ('./controllers/games-controller');
+
+const router = express.Router ();
+
+router.get ('/', (req, res) => {
+  res.json ({message: 'Hello, world!'});
+});
+
+router.get ('/games', gamesController.index);
+router.get ('/games/:id', gamesController.show);
+
+router.post ('/games', gamesController.save);
+router.post ('/games/:id/genres', gamesController.addGenre);
+
+router.put ('/games/:id', gamesController.update);
+
+router.delete ('/games/:id', gamesController.delete);
+router.delete ('/games/:id/genres/:name', gamesController.removeGenre);
+
+module.exports = router;
